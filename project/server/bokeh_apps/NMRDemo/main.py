218cea89e23d6288b4e2a9273d143cbab93ec5f2
Unknown <biggstd@gmail.com>
2017-12-13 09:23:46 -0800

    Added new bokeh application skeleton for NMR vis.

---
+++ project/server/bokeh_apps/NMRDemo/main.py
@@ -0,0 +1,45 @@
+"""
+################################
+Literature NMR Data - Bokeh Demo
+################################
+
+This application is divorced from any database. Rather, it
+simply loads a set of demo metadata files.
+
+"""
+
+# General imports
+# import os
+# import sys
+# import collections
+# import itertools
+# import json
+# Bokeh imports
+# from bokeh import events
+# from bokeh.plotting import figure
+# from bokeh.layouts import layout, widgetbox, row, column
+# from bokeh.models import ColumnDataSource, HoverTool
+# from bokeh.models.widgets import MultiSelect, Div, Paragraph, Button
+# from bokeh.palettes import linear_palette, viridis
+# from bokeh.io import curdoc
+
+def create_figure():
+	"""
+	Create the bokeh plot.
+	"""
+	pass
+
+
+def update(attr, old, new):
+	"""
+	Define the function to be run upon an update call.
+	"""
+	pass
+
+
+#------------------LAYOUT--------------------#
+
+
+#------------DOCUMENT-INITIALIZATION---------#
+curdoc().add_root()
+curdoc().title = "27 Al NMR Crossfilter"
