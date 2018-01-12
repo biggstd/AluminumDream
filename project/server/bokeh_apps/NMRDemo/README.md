4cda3e114d203ee329de783843d95d0efe0d5d05
Tyler Biggs <biggstd@gmail.com>
2018-01-11 16:00:43 -0800

    Major updates to NMR vis demo.

--- project/server/bokeh_apps/NMRDemo/README.md
+++ project/server/bokeh_apps/NMRDemo/README.md
@@ -9,3 +9,29 @@ This can be deployed in a standalone fashion for testing by running the bokeh se
 ```
 bokeh serve NMRDemo/
 ```
+
+## TODO
+
+- [x] Add a title div.
+- [x] Add a plot / figure title that updates according to the selections.
+- [ ] Format the metadata selection paragraph so it is readable / useful.
+	- [x] Explore conversion into an ISA object.
+	- [x] Move to the right hand side of the plot.
+- [ ] Filter the options available in the X and Y drop down selectors.
+- [ ] Create user-friendly strings for column names.
+- [ ] Add counter-ion functionality.
+- [ ] Add a color by drop down.
+- [ ] Add a unique marker drop down.
+- [ ] Add more data points.
+- [ ] Add a log scale toggle.
+- [ ] Add a process element.
+
+## TODO: Demo Presentation
+
+- [ ] Prepare / update poster.
+- [ ] Prepare ISA document diagram.
+
+## TODO: Yak-shaving
+
+- [ ] Update Bokeh layout elements by name, rather than nested indexes.
+- [ ] Change the demo data generation to a function.
--- project/server/bokeh_apps/NMRDemo/main.py
+++ project/server/bokeh_apps/NMRDemo/main.py
@@ -8,19 +8,21 @@ simply loads a set of demo metadata files.
 
 """
 
-# General imports
-# import os
-# import sys
-# import collections
-# import itertools
-# import json
+# General Imports
+import pandas as pd
+
 # Bokeh imports
 from bokeh.layouts import layout, widgetbox
 from bokeh.models import ColumnDataSource, Select, HoverTool, TapTool
 from bokeh.plotting import figure, curdoc
+from bokeh.models.widgets import Div
+from bokeh.palettes import Spectral5
+from bokeh.transform import factor_cmap
 
 # isaDream imports.
 from isadream.nmr_demo_sa import *
+
+COLORS = Spectral5
 
 # Simulate the return from a database query.
 # Build the investigation object.
@@ -35,10 +37,12 @@ data_frame, metadata_dict = build_data_md_pair(matching_studies)
 
 # Get the column names for use in the selectors.
 columns = sorted(data_frame.columns)
+discrete = [x for x in columns if data_frame[x].dtype == object]
+continuous = [x for x in columns if x not in discrete]
+quantileable = [x for x in continuous if len(data_frame[x].unique()) > 20]
 
 # Assign the columnDataSources.
 source = ColumnDataSource()
-# metadata = ColumnDataSource()
 
 
 def update_data():
@@ -63,12 +67,8 @@ def tap_select_callback(attr, old, new):
     select a single data point.
     """
     new_index = new['1d']['indices'][0]
-
     study_key = source.data['study_ID'][new_index]
-
-    print(study_key)
-
-    print(metadata_dict[study_key])
+    layout.children[1].children[2] = build_metadata_paragraph(study_key)
 
 
 def build_hover_tool():
@@ -94,11 +94,30 @@ def create_figure():
         width=800,
     )
 
+    if color.value != 'None':
+        colors = factor_cmap(
+            field_name=color.value,
+            palette=Spectral5,
+            factors=sorted(source.data[color.value].unique())
+        )
+    else:
+        colors = "#31AADE"
+
+    print(colors)
+    print(type(colors))
+
     fig.circle(
         source=source,
         x='x',
         y='y',
+        color=colors
     )
+
+    x_title = x_selector.value
+    y_title = y_selector.value
+
+    fig.xaxis.axis_label = x_title
+    fig.yaxis.axis_label = y_title
 
     fig.add_tools(build_hover_tool())
     fig.add_tools(TapTool())
@@ -106,12 +125,44 @@ def create_figure():
     return fig
 
 
-def update(attr, old, new):
+def update_plot(attr, old, new):
     """
     Define the function to be run upon an update call.
     """
-    layout.children[1] = create_figure()
+    layout.children[1].children[1] = create_figure()
     pass
+
+
+def format_assay_text(isa_assay_obj):
+    """Prepares the ISA assay object for easy reading in an HTML
+    format."""
+    out_str = (
+        '<strong>Publication Title</strong>: {}\n'
+        'Publication DOI: {}\n'
+        .format(
+            isa_assay_obj.publications[0].title,
+            isa_assay_obj.publications[0].doi
+        )
+    )
+    return out_str
+
+
+def build_metadata_paragraph(key=None):
+    """Constructs an HTML paragraph based on a given key."""
+    if key is None:
+        return Div(
+            text="No data point selected.",
+            width=300,
+            height=200,
+        )
+    else:
+        active_dict_entry = metadata_dict[key]
+        new_paragarph = Div(
+            text=format_assay_text(active_dict_entry),
+            width=300,
+            height=200,
+        )
+        return new_paragarph
 
 
 def callback(event):
@@ -120,20 +171,27 @@ def callback(event):
     pass
 
 
+# HTML Elements ---------------------------------------------------------------
+title_div = Div(text="<h1>Aluminate CrossFilter</h1>")
+
 # Controls and Selectors ------------------------------------------------------
 source.on_change('selected', tap_select_callback)
 
-x_selector = Select(title='X Axis', options=columns, value=columns[0])
-x_selector.on_change('value', update)
+x_selector = Select(title='X Axis', options=continuous, value=continuous[0])
+x_selector.on_change('value', update_plot)
 
-y_selector = Select(title='Y-Axis', options=columns, value=columns[1])
-y_selector.on_change('value', update)
+y_selector = Select(title='Y-Axis', options=continuous, value=continuous[1])
+y_selector.on_change('value', update_plot)
 
-controls = widgetbox([x_selector, y_selector])
+color = Select(title='Color', value='None', options=['None'] + discrete)
+color.on_change('value', update_plot)
+
+controls = widgetbox([x_selector, y_selector, color])
+
 layout = layout(
     children=[
-        [controls],
-        [create_figure()]
+        title_div,
+        [controls, create_figure(), build_metadata_paragraph()],
     ],
     sizing_mode='fixed'
 )
